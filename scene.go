af2d16b8bb5b1a721e6cf9ce961a18b87ab0c54a
Alex W <alex@blinkenlights.ca>
2013-12-17 23:45:02 -0500

    Add support for triangle geometry.

--- scene.go
+++ scene.go
@@ -19,8 +19,18 @@ func NewScene() *Scene {
 	s.LightSrc = Light{vmath.Vec3{8.0, 7.0, 0.0}, ColorWhite(), 0.2, 0.7, 0.8}
 	s.Primitives = append(s.Primitives, Primitive{Sphere{vmath.Vec3{1.0, 2.5, -13.0}, 2.5}, Material{Color{0, 0, 0.75, 1.0}, 50, false}})
 	s.Primitives = append(s.Primitives, Primitive{Sphere{vmath.Vec3{3.0, 1.0, -7.0}, 1.0}, Material{Color{0, 0.5, 0, 1.0}, 20, false}})
-	s.Primitives = append(s.Primitives, Primitive{Sphere{vmath.Vec3{-4.0, 2.0, -9.0}, 2.0}, Material{Color{0.75, 0, 0, 1.0}, 100, false}})
+	s.Primitives = append(s.Primitives, Primitive{Sphere{vmath.Vec3{-4.0, 1.5, -9.0}, 1.5}, Material{Color{0.75, 0, 0, 1.0}, 100, false}})
 	s.Primitives = append(s.Primitives, Primitive{Plane{vmath.Vec3{0.0, 1.0, 0.0}, 0.0}, Material{Color{0.5, 0.5, 0.5, 1.0}, 10, true}})
+	
+	pyrMat := Material{Color{1.0, 0.6, 0.2, 1.0}, 200, false}
+	tri1 := Triangle{[3]vmath.Vec3{vmath.Vec3{-8.0, 0.0, -18.0}, vmath.Vec3{-8.0, 8.0, -21.0}, vmath.Vec3{-12.0, 0.0, -24.0}}}
+	s.Primitives = append(s.Primitives, Primitive{tri1, pyrMat})
+
+	tri2 := Triangle{[3]vmath.Vec3{vmath.Vec3{-4.0, 0.0, -24.0}, vmath.Vec3{-8.0, 8.0, -21.0}, vmath.Vec3{-8.0, 0.0, -18.0}}}
+	s.Primitives = append(s.Primitives, Primitive{tri2, pyrMat})
+
+	tri3 := Triangle{[3]vmath.Vec3{vmath.Vec3{-12.0, 0.0, -24.0}, vmath.Vec3{-8.0, 8.0, -21.0}, vmath.Vec3{-4.0, 0.0, -24.0}}}
+	s.Primitives = append(s.Primitives, Primitive{tri3, pyrMat})
 
 	return &s
 }
@@ -34,10 +44,12 @@ func (s *Scene) BackgroundColor(r Ray) color.RGBA {
 }
 
 func (s *Scene) IsShadowed(r Ray) bool {
+	dtl := r.Origin.Sub(s.LightSrc.Position).Length()
+
 	// For every object in the scene, check if the ray hits it.
 	for _, prm := range s.Primitives {
-		intersects, _, _, _ := prm.Geometry.RayCollision(r)
-		if intersects {
+		intersects, t, _, _ := prm.Geometry.RayCollision(r)
+		if intersects && t < dtl{
 			return true
 		}
 	}
---
+++ triangle.go
@@ -0,0 +1,45 @@
+package rayngo
+
+import (
+	"github.com/malexw/vmath"
+)
+
+type Triangle struct {
+	Vertices [3]vmath.Vec3
+}
+
+func (tr Triangle) RayCollision(r Ray) (bool, float64, vmath.Vec3, vmath.Vec3) {
+	ab := tr.Vertices[1].Sub(tr.Vertices[0])
+	ac := tr.Vertices[2].Sub(tr.Vertices[0])
+	negDir := r.Direction.Scale(-1)
+
+	normal := ab.Cross(ac)
+
+	denom := negDir.Dot(normal)
+	if denom <= 0.0 {
+		return false, 0.0, vmath.Vec3{}, vmath.Vec3{}
+	}
+
+	ao := r.Origin.Sub(tr.Vertices[0])
+	t := ao.Dot(normal)
+	if t < 0.0 {
+		return false, 0.0, vmath.Vec3{}, vmath.Vec3{}
+	}
+
+	e := negDir.Cross(ao)
+	v := ac.Dot(e)
+	if v < 0.0 || v > denom {
+		return false, 0.0, vmath.Vec3{}, vmath.Vec3{}
+	}
+	w := -ab.Dot(e)
+	if w < 0.0 || v + w > denom {
+		return false, 0.0, vmath.Vec3{}, vmath.Vec3{}
+	}
+
+	denomInv := 1.0 / denom
+	t = t * denomInv
+	//v = v * denomInv
+	//w = w * denomInv
+	//u := 1.0 - v - w
+	return true, t, r.Origin.Add(r.Direction.Scale(t)), normal.Normalize()
+}
